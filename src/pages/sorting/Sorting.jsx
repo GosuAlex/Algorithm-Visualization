1b7d4f3d4d515de43de8d5f840eca24051ce5dcd
GosuAlex <super.atx@gmail.com>
2020-02-26 14:40:52 +0100

    importing sort algos

--- src/pages/sorting/Sorting.jsx
+++ src/pages/sorting/Sorting.jsx
@@ -1,5 +1,4 @@
 // put speed control
-// import sort algos instead
 
 import React, { useState } from "react";
 
@@ -28,46 +27,6 @@ const Sorting = () => {
     const newArr = Array.from({ length: arrSize }, () => random());
     setArrState(newArr);
   };
-
-  // const insertionSort = (arr) => {
-  //   // setArr(Object.assign([...arr], {0 : arr[1], 1 : arr[0]})); // test one line set new array with swapped indexes. But it added whole thing.
-  //   // Replay object holds all the event records of this sorting algo.
-  //   // It will be playing back for DOM later to avoid setTimeout unfortunate changing variables behavior.  
-  //   const sortingReplay = {
-  //     arrMutation: [],
-  //     indexMovement: [],
-  //     swapMovement: [],
-  //     frontIndex: []
-  //   };
-
-  //   for (let counter = 1; counter < arr.length; counter++) {
-  //     let greenLight = true;
-  //     let index = counter;
-
-  //       while (index >= 1 && greenLight) {
-  //         sortingReplay.indexMovement.push(index);
-  //         // if behind index is bigger than current index
-  //         if (arr[index - 1] > arr[index]) {
-  //           // Destructure. Change places in this case.
-  //           [arr[index - 1], arr[index]] = [arr[index], arr[index - 1]];
-  //           sortingReplay.swapMovement.push(index - 1);
-  //           index--;
-  //         } else {            
-  //           sortingReplay.swapMovement.push(null);
-  //           greenLight = false;
-  //         }
-  //         sortingReplay.arrMutation.push([...arr]);
-  //         sortingReplay.frontIndex.push(counter+1);
-  //       }
-  //   }
-
-  //   sortingReplay.arrMutation.push([...arr]);
-  //   sortingReplay.swapMovement.push(null);
-  //   sortingReplay.indexMovement.push(null);
-  //   sortingReplay.frontIndex.push(null);
-    
-  //   return sortingReplay;
-  // };
 
   const sort = () => {
     setPlaying(true);
